b090704ec49bceeb251d592d07116fb8b51aca5a
Tentei06 <tentei06@gmail.com>
2026-05-14 11:37:18 -0600

    Created pseudocode

---
+++ src/RecursiveProduct.java
@@ -0,0 +1,23 @@
+/*
+
+Program Name: RecursiveProduct.java
+Author: Cody Walker
+Date: 5/14/2026
+
+Pseudocode:
+
+Create scanner object.
+Create array to store 5 numbers.
+Ask user to enter 5 numbers.
+Store numbers into array.
+Call recursive product method.
+Multiply numbers recursively.
+Display final product.
+
+Program Inputs:
+User enters 5 numbers
+
+Program Outputs:
+Product of the 5 numbers
+
+*/
